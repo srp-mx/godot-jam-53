30b6b53bda7836b754d2faf18f9281c9a1b0e12a
Santiago Romero <srp.devel@gmail.com>
2023-01-20 12:54:56 -0600

    Postmerge

--- Main_m.cs
+++ Main_m.cs
@@ -5,7 +5,7 @@ using GodotPlugins.Game;
 public partial class Main_m : Control
 {
 	// Loaded scenes
-	private PackedScene Game;
+	private PackedScene game;
 	
 	// Loaded buttons?
 	private Button Play;
@@ -13,14 +13,14 @@ public partial class Main_m : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Game = GD.Load<PackedScene>("res://sections.tscn");
+		game = GD.Load<PackedScene>("res://scenes/prefabs/sections.tscn");
 		
 		Play = GetNode<Button>("Container/Play");
 		Play.Pressed += PlayOnPressed;
 	}
 	private void PlayOnPressed()
 	{
-		GetTree().ChangeSceneToPacked(Game);
+		GetTree().ChangeSceneToPacked(game);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
