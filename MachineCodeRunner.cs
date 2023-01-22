03c984e1625be7d9d5bb2cd864ddb8475ea7c2db
srp-mx <111163827+srp-mx@users.noreply.github.com>
2023-01-21 21:01:25 -0600

    Imported meshes

--- MachineCodeRunner.cs
+++ MachineCodeRunner.cs
@@ -8,68 +8,68 @@ using Game.Assembly;
 
 public partial class Machine : Node
 {
-    IEnumerator currInstruction;
-    int instructionPtr;
+	IEnumerator currInstruction;
+	int instructionPtr;
 
-    bool ended = false;
-    Task<bool> task;
+	bool ended = false;
+	Task<bool> task;
 
-    public void StepCode()
-    {
-        if (!CanRun())
-            return;
+	public void StepCode()
+	{
+		if (!CanRun())
+			return;
 
-        task = Task.Factory.StartNew<bool>(() => 
-        {
-            bool wouldContinue = currInstruction.MoveNext();
-            instructionPtr = (int)currInstruction.Current;
-            registers[(int)Register.IP] = instructionPtr;
-            debugLog("Moved iptr to " + instructionPtr);
-            if (!wouldContinue)
-            {
-                debugLogMem();
-                codeLog(code.GetExitStatus().ToString());   
-            }
-            return wouldContinue;
-        });
-    }
+		task = Task.Factory.StartNew<bool>(() => 
+		{
+			bool wouldContinue = currInstruction.MoveNext();
+			instructionPtr = (int)currInstruction.Current;
+			registers[(int)Register.IP] = instructionPtr;
+			debugLog("Moved iptr to " + instructionPtr);
+			if (!wouldContinue)
+			{
+				debugLogMem();
+				codeLog(code.GetExitStatus().ToString());   
+			}
+			return wouldContinue;
+		});
+	}
 
-    private bool CanRun()
-    {
-        // we have to compile and get it first
-        if (currInstruction is null)
-            return false;
+	private bool CanRun()
+	{
+		// we have to compile and get it first
+		if (currInstruction is null)
+			return false;
 
-        // if the code is valid
-        if (codeRejected)
-            return false;
+		// if the code is valid
+		if (codeRejected)
+			return false;
 
-        if (!finishedCompile)
-            return false;
+		if (!finishedCompile)
+			return false;
 
-        // can't be overheated D:
-        if (OnCooldown)
-            return false;
+		// can't be overheated D:
+		if (OnCooldown)
+			return false;
 
-        // if the program ended
-        if (ended)
-            return false;
+		// if the program ended
+		if (ended)
+			return false;
 
-        // if there's an instruction running, return
-        // additionally if it's completed, set ended flag
-        if (task is not null)
-        {
-            if (!task.IsCompleted)
-                return false;
+		// if there's an instruction running, return
+		// additionally if it's completed, set ended flag
+		if (task is not null)
+		{
+			if (!task.IsCompleted)
+				return false;
 
-            ended = !task.Result;
-            if (ended) codeLog("[ENDED PROGRAM]");
-            else task = null; // making this null allows continuous running, since the task's 
-                              // IsCompleted attribute gets wiped
-            return false;
-        }
+			ended = !task.Result;
+			if (ended) codeLog("[ENDED PROGRAM]");
+			else task = null; // making this null allows continuous running, since the task's 
+							  // IsCompleted attribute gets wiped
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
 }
